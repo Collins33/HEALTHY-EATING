8b585d19e9a185f3532ec0d921a50d15775d8b34
collins njau <collinsnjau39@gmail.com>
2019-04-24 13:30:23 +0300

    Add unit tests

---
+++ tests/bookControllerTests.js
@@ -0,0 +1,27 @@
+const should = require('should');
+const sinon = require('sinon');
+const booksController = require('../controllers/booksController');
+
+describe('Book controller testing', () => {
+  describe('Post', () => {
+    it('should not allow an empty title', () => {
+      const Book = function (book) { this.save = () => {}; };
+      const req = {
+        body: {
+          author: 'Collins',
+        },
+      };
+      const res = {
+        status: sinon.spy(),
+        send: sinon.spy(),
+        json: sinon.spy(),
+      };
+
+      const controller = booksController(Book);
+      controller.post(req, res);
+
+      res.status.calledWith(400).should.equal(true);
+      res.send.calledWith('Title is required').should.equal(true);
+    });
+  });
+});
