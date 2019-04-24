643d049934b5ce11319dc26bc5cd29b6294a3b6f
collins njau <collinsnjau39@gmail.com>
2019-04-24 10:17:16 +0300

    Add http verbs - add put - add patch - add delete

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable no-param-reassign */
 const express = require('express');
 
@@ -23,29 +24,61 @@ function routes(Book) {
       });
     });
 
+  // add the middleware
+  // check if the book exists
+  bookRouter.use('/books/:bookId', (req, res, next) => {
+    const { bookId } = req.params;
+    Book.findById(bookId, (err, book) => {
+      if (err) {
+        return res.send(err);
+      }
+      // if the book exists, pass it downstream to the routes
+      if (book) {
+        req.book = book;
+        return next();
+      }
+      // if book is not found return 404
+      return res.sendStatus(404);
+    });
+  });
   bookRouter.route('/books/:bookId')
     .get((req, res) => {
-    // get bookid from url
-      const { bookId } = req.params;
-      Book.findById(bookId, (err, book) => {
+      res.json(req.book);
+    })
+    .put((req, res) => {
+      const { book } = req;
+      book.title = req.body.title;
+      book.author = req.body.author;
+      book.genre = req.body.genre;
+      book.read = req.body.read;
+      book.save();
+      return res.json(book);
+    })
+    .patch((req, res) => {
+      const { book } = req;
+      // get an array from req.body
+      // will return an array we can loop over
+      if (req.body._id) {
+        delete req.body._id;
+      }
+      Object.entries(req.body).forEach((item) => {
+        const key = item[0];
+        const value = item[1];
+        book[key] = value;
+      });
+      req.book.save((err) => {
         if (err) {
           return res.send(err);
         }
         return res.json(book);
       });
     })
-    .put((req, res) => {
-      const { bookId } = req.params;
-      Book.findById(bookId, (err, book) => {
+    .delete((req, res) => {
+      req.book.remove((err) => {
         if (err) {
           return res.send(err);
         }
-        book.title = req.body.title;
-        book.author = req.body.author;
-        book.genre = req.body.genre;
-        book.read = req.body.read;
-        book.save();
-        return res.json(book);
+        return res.status(204);
       });
     });
 
